d722204f742df10df7ebb4d8c07ef8714a3abef0
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2016-08-28 07:49:21 -0500

    More encapsualtion

--- src/main/java/hm/binkley/layers/Character.java
+++ src/main/java/hm/binkley/layers/Character.java
@@ -1,6 +1,9 @@
 package hm.binkley.layers;
 
 import hm.binkley.layers.Field.StringField;
+import hm.binkley.layers.Layers.NewLayers;
+
+import static hm.binkley.layers.Layers.newLayers;
 
 /**
  * {@code Character} <b>needs documentation</b>.
@@ -9,23 +12,21 @@ import hm.binkley.layers.Field.StringField;
  * @todo Needs documentation.
  */
 public final class Character {
-    private final Layers layers = new Layers();
+    private final Layers layers;
     private Layer current;
 
-    {
-        layers.add("player", new StringField());
-        layers.add("name", new StringField());
-    }
-
     public Character(final String player, final String name) {
-        current = layers.
-                newLayer(surface -> new Base(surface, player, name)).
-                commit(BlankLayer::new);
+        final NewLayers<BaseLayer> initial = newLayers(
+                surface -> new BaseLayer(surface, player, name));
+        layers = initial.layers;
+        layers.addRule("player", new StringField());
+        layers.addRule("name", new StringField());
+        current = initial.firstLayer.commit(BlankLayer::new);
     }
 
-    public static final class Base
+    public static final class BaseLayer
             extends BlankLayer {
-        public Base(final Surface surface, final String player,
+        public BaseLayer(final Surface surface, final String player,
                 final String name) {
             super(surface);
             put("player", player);
--- src/main/java/hm/binkley/layers/Layers.java
+++ src/main/java/hm/binkley/layers/Layers.java
@@ -1,16 +1,18 @@
 package hm.binkley.layers;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static java.util.Collections.unmodifiableMap;
+import static lombok.AccessLevel.PRIVATE;
 
 /**
  * {@code Layers} <b>needs documentation</b>.
@@ -18,11 +20,39 @@ import static java.util.Collections.unmodifiableMap;
  * @author <a href="mailto:binkley@alumni.rice.edu">B. K. Oxley (binkley)</a>
  * @todo Needs documentation.
  */
+@RequiredArgsConstructor(access = PRIVATE)
 public final class Layers {
     private final Collection<Layer> layers = new ConcurrentLinkedQueue<>();
     private final Map<String, Field> fields = new ConcurrentHashMap<>();
     private final ConcurrentMap<String, Object> cache
             = new ConcurrentHashMap<>();
+
+    @RequiredArgsConstructor
+    public static final class NewLayers<L extends Layer> {
+        public final Layers layers;
+        public final L firstLayer;
+    }
+
+    public static <L extends Layer> NewLayers<L> newLayers(
+            final Function<Surface, L> ctor) {
+        final Layers layers = new Layers();
+        return new NewLayers<>(layers, layers.newLayer(ctor));
+    }
+
+    public <T> Layers addRule(final String key, final Field<T> field) {
+        fields.put(key, field);
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> Field<T> ruleFor(final String key) {
+        return fields.getOrDefault(key, Field.LAST);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(final String key) {
+        return (T) cache.get(key);
+    }
 
     public Map<String, Object> committed() {
         return unmodifiableMap(cache);
@@ -33,40 +63,34 @@ public final class Layers {
                 map(Layer::changes);
     }
 
-    public Layers add(final String key, final Field field) {
-        fields.put(key, field);
-        return this;
+    private <L extends Layer> L newLayer(final Function<Surface, L> ctor) {
+        return ctor.apply(new LayersSurface());
     }
 
-    public <L extends Layer> L newLayer(final Function<Surface, L> ctor) {
-        return ctor.apply(new Surface() {
-            @Override
-            public void commit(final Layer layer) {
-                layers.add(layer);
-                merge(cache, layer.changes());
-            }
+    private final class LayersSurface
+            implements Surface {
+        @Override
+        public void commit(final Layer layer) {
+            layers.add(layer);
+            merge(cache, layer.changes());
+        }
 
-            @Override
-            public Map<String, Object> changes(final Layer layer) {
-                final Map<String, Object> whatIf = new HashMap<>(cache);
-                merge(whatIf, layer.changes());
-                return unmodifiableMap(whatIf);
-            }
-        });
-    }
+        @Override
+        public Map<String, Object> changes(final Layer layer) {
+            final Map<String, Object> whatIf = new HashMap<>(cache);
+            merge(whatIf, layer.changes());
+            return unmodifiableMap(whatIf);
+        }
 
-    private BiFunction<Object, Object, Object> ruleFor(final String key) {
-        return fields.getOrDefault(key, Field.LAST).rule;
-    }
+        private void merge(final Map<String, Object> that,
+                final Map<String, Object> map) {
+            map.entrySet().
+                    forEach(e -> mergeOne(that, e.getKey(), e.getValue()));
+        }
 
-    private void merge(final Map<String, Object> that,
-            final Map<String, Object> map) {
-        map.entrySet().
-                forEach(e -> mergeOne(that, e.getKey(), e.getValue()));
-    }
-
-    private void mergeOne(final Map<String, Object> map, final String key,
-            final Object value) {
-        map.merge(key, value, ruleFor(key));
+        private void mergeOne(final Map<String, Object> map, final String key,
+                final Object value) {
+            map.merge(key, value, ruleFor(key).rule);
+        }
     }
 }
