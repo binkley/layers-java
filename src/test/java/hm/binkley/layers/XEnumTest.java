b76a04dfb268855b2a382dc53d55983a90e0b6d6
Brian Oxley <andrew.dean+brian.oxley@thoughtworks.com>
2016-10-06 12:29:33 -0500

    Raise test coverage to 15%

---
+++ src/test/java/hm/binkley/layers/XEnumTest.java
@@ -0,0 +1,50 @@
+package hm.binkley.layers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static hm.binkley.layers.XEnumTest.Eg.A;
+import static hm.binkley.layers.XEnumTest.Eg.B;
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableList;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class XEnumTest {
+    @Test
+    void shouldBeNamedAfterThemselves() {
+        assertAll(() -> assertEquals("A", A.name()),
+                () -> assertEquals("B", B.name()));
+    }
+
+    @Test
+    void shouldOrdinateStepwiseFromZero() {
+        assertAll(() -> assertEquals(0, A.ordinal()),
+                () -> assertEquals(1, B.ordinal()));
+    }
+
+    @Test
+    void shouldIterateInOrdinalOrder() {
+        assertEquals(asList(A, B), Eg.values());
+    }
+
+    static final class Eg
+            extends XEnum<Eg> {
+        private static final AtomicInteger ordinal = new AtomicInteger();
+        private static final List<Eg> values = new ArrayList<>();
+        static final Eg A = new Eg("A");
+        static final Eg B = new Eg("B");
+
+        static List<Eg> values() {
+            return unmodifiableList(values);
+        }
+
+        private Eg(final String name) {
+            super(name, ordinal.getAndIncrement());
+            values.add(this);
+        }
+    }
+}
