33ee3c8d0a116323d01fb0f281007d0428159412
Brian Oxley <boxley@thoughtworks.com>
2016-12-12 14:18:30 -0600

    Hygiene

--- layers-domain/src/main/java/hm/binkley/layers/dnd/item/AttunementItem.java
+++ layers-domain/src/main/java/hm/binkley/layers/dnd/item/AttunementItem.java
@@ -9,13 +9,11 @@ import java.util.function.Function;
 
 import static hm.binkley.layers.dnd.item.Attunement.attune;
 
-/** @todo Real values for weight/volume */
 public class AttunementItem<L extends AttunementItem<L>>
         extends MagicItem<L> {
     public AttunementItem(final LayerSurface layers, final String name,
             final String description, final Type type, final Rarity rarity,
             final Weight weight, final Volume volume, final String notes) {
-        // TODO: Why is static method imports not compiling here?
         super(layers, name, description, type, rarity, weight, volume, true,
                 notes);
     }
@@ -38,8 +36,8 @@ public class AttunementItem<L extends AttunementItem<L>>
 
     @Override
     public L put(final Object key, final Object value) {
-        return value instanceof Rule ? super
-                .put(key, new AttunementItemRule<>(this, (Rule<?>) value))
-                : super.put(key, value);
+        return super.put(key,
+                value instanceof Rule ? new AttunementItemRule<>(this,
+                        (Rule<?>) value) : value);
     }
 }
