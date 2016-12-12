de3fe2c3ca1c51c530dd4852ae685df6db27c43a
Brian Oxley <boxley@thoughtworks.com>
2016-12-12 14:19:25 -0600

    Hygiene

--- layers-domain/src/main/java/hm/binkley/layers/dnd/item/MagicItem.java
+++ layers-domain/src/main/java/hm/binkley/layers/dnd/item/MagicItem.java
@@ -2,14 +2,12 @@ package hm.binkley.layers.dnd.item;
 
 import hm.binkley.layers.Layers.LayerSurface;
 
-/** @todo Real values for weight/volume */
 public class MagicItem<L extends MagicItem<L>>
         extends Item<L> {
     public MagicItem(final LayerSurface layers, final String name,
             final String description, final Type type, final Rarity rarity,
             final Weight weight, final Volume volume,
             final boolean attunement, final String notes) {
-        // TODO: Why is static method imports not compiling here?s
         super(layers, name, description, type, weight, volume, notes);
         putDetail(Attunement.class, attunement).
                 putDetail(Rarity.class, rarity);
